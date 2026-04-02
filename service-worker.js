a27b31fecb3c4a5504ba88c74641861a0a0519e1
henryehammers-dotcom <henryehammers@gmail.com>
2026-04-02 14:49:51 -0400

    committing files to main

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE = 'tallymarket-v1';
+const ASSETS = [
+  '/meal-prep-log/',
+  '/meal-prep-log/index.html',
+  '/meal-prep-log/manifest.json',
+  '/meal-prep-log/icon-192.png',
+  '/meal-prep-log/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
